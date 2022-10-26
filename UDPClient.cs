70af7084208fb024de674b1b89ac039000a151aa
Tran Quoc Khanh <91265666+tranquockhanhhh@users.noreply.github.com>
2022-10-26 23:13:29 +0700

    Add files via upload

---
+++ UDPClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class SimpleUdpClient
+{
+    public static void Main()
+    {
+        Socket client = new Socket(AddressFamily.InterNetwork,
+                                    SocketType.Dgram, ProtocolType.Udp);
+
+        EndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+        //Note -- need to use EndPoint for the ReceiveFrom to work!
+
+        byte[] data;
+        string input;
+        int recv;
+
+
+        while (true)
+        {
+            Console.Write("Enter message for server or exit to stop: ");
+            input = Console.ReadLine();
+            if (input == "exit")
+                break;
+            client.SendTo(Encoding.ASCII.GetBytes(input), remoteEP);
+            data = new byte[1024];
+            recv = client.ReceiveFrom(data, ref remoteEP);
+            Console.Write("Echo from from {0}: ", remoteEP.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+        }
+        Console.WriteLine("Stopping client");
+        client.Close();
+    }
+}
---
+++ UDPServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UdpSocketServer
+{
+    public static void Main()
+    {
+        IPEndPoint localEP = new IPEndPoint(IPAddress.Any, 9050);
+
+        Socket server = new Socket(AddressFamily.InterNetwork,
+                 SocketType.Dgram, ProtocolType.Udp);
+
+        server.Bind(localEP);
+        Console.WriteLine("Waiting for a client...");
+
+        //dummy end-point
+        EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+        int recv;
+        byte[] data;
+
+        while (true)
+        {
+            data = new byte[1024];
+            recv = server.ReceiveFrom(data, ref remoteEP);
+            Console.Write("Received from {0}: ", remoteEP.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+            server.SendTo(data, recv, SocketFlags.None, remoteEP);
+        }
+    }
+}
